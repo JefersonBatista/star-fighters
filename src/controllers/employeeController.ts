7bff070766e11509b727bc9998fdf2a3a29b553f
Mateus De Nardo <mateusdnm@hotmail.com>
2022-04-08 12:14:48 +0100

    refactor: change js to ts

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -1,25 +1,26 @@
+import { Request, Response } from "express";
 import { employeeService } from "../services/employeeService.js";
 
-export async function getEmployees(req, res) {
+export async function getEmployees(req: Request, res: Response) {
   const employees = await employeeService.findAll();
   res.send(employees);
 }
 
-export async function getEmployee(req, res) {
+export async function getEmployee(req: Request, res: Response) {
   const { id } = req.params;
 
   const employee = await employeeService.findById(id);
   res.send(employee);
 }
 
-export async function getEmployeeNetSalary(req, res) {
+export async function getEmployeeNetSalary(req: Request, res: Response) {
   const { id } = req.params;
 
   const netSalaryWithTaxes = await employeeService.getNetSalaryWithTaxes(id);
   res.send(netSalaryWithTaxes);
 }
 
-export async function getEmployeeVacation(req, res) {
+export async function getEmployeeVacation(req: Request, res: Response) {
   const { id } = req.params;
   const { isSelling, startDate, vacationDaysAmount } = req.body;
 
@@ -32,14 +33,14 @@ export async function getEmployeeVacation(req, res) {
   res.send(vacation);
 }
 
-export async function insertEmployee(req, res) {
+export async function insertEmployee(req: Request, res: Response) {
   const employee = req.body;
 
   await employeeService.insert(employee);
   res.sendStatus(201);
 }
 
-export async function updateEmployee(req, res) {
+export async function updateEmployee(req: Request, res: Response) {
   const { id } = req.params;
   const employee = req.body;
 
@@ -47,7 +48,7 @@ export async function updateEmployee(req, res) {
   res.sendStatus(200);
 }
 
-export async function deleteEmployee(req, res) {
+export async function deleteEmployee(req: Request, res: Response) {
   const { id } = req.params;
 
   await employeeService.remove(id);
